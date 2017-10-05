77b1064195ae1a8a98395daa6c90867078dd13ab
Tyler Truong <tylertruong@gmail.com>
2017-10-04 18:25:52 -0700

    Minor fix to change single quotes.

--- helper/config/config.js
+++ helper/config/config.js
@@ -1,1 +1,1 @@
-global.STOCK_API_KEY = "GWH3E7F1KD3NH816";//duplicate this file into config.js with proper key
+global.STOCK_API_KEY = 'GWH3E7F1KD3NH816';//duplicate this file into config.js with proper key
