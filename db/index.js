8106228a7b05216ca69221c6bd617262d432a323
Andrew Rossman <andrew100r@gmail.com>
2017-10-06 14:31:00 -0700

    (cleanup) Fix merge conflict.

--- db/index.js
+++ db/index.js
@@ -11,8 +11,8 @@ let connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
-let getPortfolio = () => {
-  let queryString = `SELECT * FROM positions`;
+let getPortfolio = (user) => {
+  let queryString = `SELECT * FROM positions WHERE trader = user`;
   return new Promise((resolve, reject) => {
     connection.query(queryString, (err, res) => {
       if (!err) {
@@ -25,38 +25,62 @@ let getPortfolio = () => {
 };
 
 let saveStock = (stock) => {
-  let eventQuery = `INSERT INTO events (trader, symbol, event_type, price, volume, date_time) VALUES ('Gordon', ${mysql.escape(stock.symbol)}, 'B', ${stock.close}, 1, ${mysql.escape(stock.refresh)});`;
+  let eventQuery = `INSERT INTO events (trader, symbol, event_type, price, volume, date_time) VALUES (${stock.trader}, ${mysql.escape(stock.symbol)}, 'B', ${stock.close}, ${stock.volume}, ${mysql.escape(stock.refresh)});`;
   return new Promise((resolve, reject) => {
     connection.query(eventQuery, (err, res) => {
       if (!err) {
-        let positionsQuery = `INSERT INTO positions (trader, symbol, number_of_shares, time_of_last_event, total_cost, net_gain) VALUES ('Gordon', ${mysql.escape(stock.symbol)}, 1, ${mysql.escape(stock.refresh)}, ${stock.close}, 0) ON DUPLICATE KEY UPDATE number_of_shares = 1, time_of_last_event = ${mysql.escape(stock.refresh)}, total_cost = ${stock.close}, net_gain = 0`;
-        connection.query(positionsQuery, (err, res) => {
+        connection.query(`SELECT * FROM positions WHERE trader = ${stock.trader} & symbol = ${mysql.escape(stock.symbol)}`, (err, res) => {
           if (!err) {
-            resolve(res);
+            // Update pre-existing position
+            let currentShareNum = res[0].number_of_shares;
+            let updatedShareNum = currentShareNum + stock.volume;
+            let currentTotalCost = res[0].total_cost;
+            let updatedTotalCost = currentTotalCost + (stock.volume * stock.close);
+            let postionsUpdate = `UPDATE positions SET number_of_shares = ${updatedShareNum}, time_of_last_event = ${mysql.escape(stock.refresh)}, total_cost = ${updatedTotalCost} WHERE trader = ${stock.trader} & symbol = ${mysql.escape(stock.symbol)}`;
+            connection.query(postionsUpdate, (err, res) => {
+              if (!err) {
+                resolve(res);
+              } else {
+                reject(err);
+              }
+            });
           } else {
-            reject(err);
+            // Insert new position
+            let totalCost = stock.volume * stock.close;
+            let positionsInsert = `INSERT INTO positions (trader, symbol, number_of_shares, time_of_last_event, total_cost, net_gain) VALUES (${stock.trader}, ${mysql.escape(stock.symbol)}, ${stock.volume}, ${mysql.escape(stock.refresh)}, ${totalCost}, 0);`;
+            connection.query(positionsInsert, (err, res) => {
+              if (!err) {
+                resolve(res);
+              } else {
+                reject(err);
+              }
+            });
           }
         });
       } else {
         reject(err);
-      }
+      }  
     });
   });
 };
 
 
 let sellStock = (stock) => {
-  let eventQuery = `INSERT INTO events (trader, symbol, event_type, price, volume, date_time) VALUES ('Gordon', ${mysql.escape(stock.symbol)}, 'S', ${stock.close}, 1, ${mysql.escape(stock.refresh)});`;
+  let eventQuery = `INSERT INTO events (trader, symbol, event_type, price, volume, date_time) VALUES (${stock.trader}, ${mysql.escape(stock.symbol)}, 'S', ${stock.close}, ${stock.volume}, ${mysql.escape(stock.refresh)});`;
   return new Promise((resolve, reject) => {
     connection.query(eventQuery, (err, res) => {
       if (!err) {
-        let currentPositionValue = `SELECT total_cost FROM positions WHERE symbol = ${mysql.escape(stock.symbol)}`;
-        connection.query(currentPositionValue, (err, res) => {
+        connection.query(`SELECT * FROM positions WHERE trader = ${stock.trader} & symbol = ${mysql.escape(stock.symbol)}`, (err, res) => {
           if (!err) {
-            let positionGain = stock.close - res[0].total_cost;
-            console.log('position gain', positionGain);
-            let positionQuery = `UPDATE positions SET number_of_shares=0, time_of_last_event=${mysql.escape(stock.refresh)}, total_cost=0, net_gain=${positionGain} WHERE symbol=${mysql.escape(stock.symbol)}`;
-            connection.query(positionQuery, (err, res) => {
+            let avgShareCost = res[0].total_cost / res[0].number_of_shares;
+            let costOfSale = avgShareCost * stock.volume;
+            let incomeFromSale = stock.close * stock.volume;
+            let saleNetGain = incomeFromSale - costOfSale;
+            let updatedNetGain = res[0].net_gain + saleNetGain;
+            let updatedTotalCost = res[0].total_cost - costOfSale;
+            let updatedShareNum = res[0].number_of_shares - stock.volume;
+            let positionsUpdate = `UPDATE positions SET number_of_shares = ${updatedShareNum}, time_of_last_event = ${mysql.escape(stock.refresh)}, total_cost = ${updatedTotalCost}, net_gain = ${updatedNetGain} WHERE trader = ${stock.trader} & symbol = ${stock.symbol}`;
+            connection.query(positionsUpdate, (err, res) => {
               if (!err) {
                 resolve(res);
               } else {
--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE events (
   event_type CHAR(1) NOT NULL,
   price DECIMAL(10,4) NOT NULL,
   volume INT NOT NULL,
-  date_time TIMESTAMP NOT NULL,
+  date_time DATETIME NOT NULL,
   PRIMARY KEY(ID)
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE positions (
 	trader VARCHAR(50) NOT NULL,	
   symbol VARCHAR(7) NOT NULL,
   number_of_shares INT NOT NULL,
-  time_of_last_event TIMESTAMP NOT NULL,
+  time_of_last_event DATETIME NOT NULL,
   total_cost DECIMAL(10, 4) NOT NULL,
   net_gain DECIMAL(10, 4) NOT NULL,
   PRIMARY KEY(trader, symbol)
